1670ef10437f317e1034cd7829a9ffd236fe3088
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2020-07-22 12:44:20 +0300

    Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,21 @@
 <template>
   <section class="container container-small mt-40">
-    <h1>
-      Jesse trades info
-    </h1>
+    <header class="about-header">
+      <h1>
+        Jesse trades info
+      </h1>
+
+      <a
+        class="text-dark"
+        href="https://github.com/nicolay-zlobin/jesse-trades-info"
+        target="_blank">
+        <githubIcon width="24px"/>
+      </a>
+    </header>
 
     <p>
       This tool is supposed to work with <a href="https://jesse-ai.com/" target="_blank">Jesse</a>'s JSON files output
-      after backtest that you can get with a command like
-      this:
+      after backtest that you can get with a command like this:
     </p>
 
     <pre class="mb-10">jesse backtest 2020-03-01 2020-07-01 --json</pre>
@@ -15,17 +23,32 @@
     <p>
       It helps parse JSON backtest files with trades and shows in a convenient way with tables and charts.
     </p>
+
+    <p>
+      If you have any questions or feature requests, you may ask it
+      <a
+        target="_blank"
+        href="https://forum.jesse-ai.com/d/127-jesse-trades-info-tool-to-review-backtest-results">here</a>.
+    </p>
   </section>
 </template>
 
 <style scoped lang="scss">
+  .about-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
 
 <script>
+import githubIcon from '@/static/svg/github.svg'
 
 export default {
   name: 'About',
-  components: {},
+  components: {
+    githubIcon
+  },
   data () {
     return {}
   },
