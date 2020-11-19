f2907501d4cb2f4184aff6b7c2299821dc4a47b4
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2020-11-19 16:45:29 +0300

    Save settings in cookies

--- plugins/persistedState.client.js
+++ plugins/persistedState.client.js
@@ -1,7 +1,25 @@
 import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
 
-export default ({ store }) => {
+export default ({ store, req }) => {
   createPersistedState({
-    paths: ['settings']
+    key: 'jti',
+    paths: ['settings'],
+    storage: {
+      getItem: (key) => {
+        // See https://nuxtjs.org/guide/plugins/#using-process-flags
+        if (process.server) {
+          const parsedCookies = cookie.parse(req.headers.cookie)
+          return parsedCookies[key]
+        }
+        else {
+          return Cookies.get(key)
+        }
+      },
+      // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+      setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: false }),
+      removeItem: key => Cookies.remove(key)
+    }
   })(store)
 }
