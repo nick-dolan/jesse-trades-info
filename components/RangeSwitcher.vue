01e28ff539facb4ead6c2cf3d072dd52dd720943
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2020-11-26 17:37:01 +0300

    Move timeframes to file, clean up

--- components/RangeSwitcher.vue
+++ components/RangeSwitcher.vue
@@ -22,6 +22,7 @@
 <script>
 
 import { mapState } from 'vuex'
+import timeframes from 'static/timeframes'
 
 export default {
   name: 'RangeSwitcher',
@@ -46,56 +47,7 @@ export default {
   data () {
     return {
       // Supported timeframes are 1m, 3m, 5m, 15m, 30m, 1h, 2h, 3h, 4h, 6h, 8h, 1D
-      timeframes: [
-        {
-          value: 1,
-          name: '1m'
-        },
-        {
-          value: 3,
-          name: '3m'
-        },
-        {
-          value: 5,
-          name: '5m'
-        },
-        {
-          value: 15,
-          name: '15m'
-        },
-        {
-          value: 30,
-          name: '30m'
-        },
-        {
-          value: 60,
-          name: '1h'
-        },
-        {
-          value: 120,
-          name: '2h'
-        },
-        {
-          value: 180,
-          name: '3h'
-        },
-        {
-          value: 240,
-          name: '4h'
-        },
-        {
-          value: 360,
-          name: '6h'
-        },
-        {
-          value: 480,
-          name: '8h'
-        },
-        {
-          value: 1440,
-          name: '1D'
-        }
-      ],
+      timeframes,
       selectedTimeframe: this.calculatedTimeframe,
       loading: false
     }
---
+++ static/timeframes.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    value: 1,
+    name: '1m'
+  },
+  {
+    value: 3,
+    name: '3m'
+  },
+  {
+    value: 5,
+    name: '5m'
+  },
+  {
+    value: 15,
+    name: '15m'
+  },
+  {
+    value: 30,
+    name: '30m'
+  },
+  {
+    value: 60,
+    name: '1h'
+  },
+  {
+    value: 120,
+    name: '2h'
+  },
+  {
+    value: 180,
+    name: '3h'
+  },
+  {
+    value: 240,
+    name: '4h'
+  },
+  {
+    value: 360,
+    name: '6h'
+  },
+  {
+    value: 480,
+    name: '8h'
+  },
+  {
+    value: 1440,
+    name: '1D'
+  }
+]
